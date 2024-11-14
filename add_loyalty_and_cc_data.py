468be6e18653c7df34850552977b285df78d2fc2
nicol <nicolas.mahn@gmx.de>
2024-11-14 12:48:19 +0100

    updated num_purchases per location

--- add_loyalty_and_cc_data.py
+++ add_loyalty_and_cc_data.py
@@ -16,13 +16,20 @@ def main():
     cc_data = load_data.open_csv_file("data/cc_data.csv")
     loyalty_data = load_data.open_csv_file("data/loyalty_data.csv")
 
-    mongo = DB("num_purchases_per_location")#
+    mongo = DB("num_purchases_per_location")
+    mongo.delete_many({})
     numb_purchases_per_location_cc = get_purchases_per_location(cc_data)
-    print(mongo.insert_one_replace_if_exists({"cc_data": numb_purchases_per_location_cc}, "cc_data"))
-    print(mongo.find({"cc_data": {"$exists": True}}))
     numb_purchases_per_location_loyalty = get_purchases_per_location(loyalty_data)
-    print(mongo.insert_one_replace_if_exists({"loyalty_data": numb_purchases_per_location_loyalty}, "loyalty_data"))
-    print(mongo.find({"loyalty_data": {"$exists": True}}))
+
+    numb_purchases_per_location = []
+    for location in (numb_purchases_per_location_loyalty.keys() | numb_purchases_per_location_cc.keys()):
+        numb_purchases_per_location.append(
+            {"location": location,
+             "numb_purchases_cc": numb_purchases_per_location_cc.get(location, 0),
+             "numb_purchases_loyalty": numb_purchases_per_location_loyalty.get(location, 0)})
+
+    print(mongo.insert_many(numb_purchases_per_location))
+    print(mongo.find_all())
 
 
 if __name__ == "__main__":
